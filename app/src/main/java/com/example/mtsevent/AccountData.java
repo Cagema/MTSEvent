034e91c0bdef9afb0f3bfbdd6caa9506584b7622
H4dDock <maxolo.di@gmail.com>
2019-03-23 04:35:20 +0300

    Enter registration layout, design it.

---
+++ app/src/main/java/com/example/mtsevent/AccountData.java
@@ -0,0 +1,18 @@
+package com.example.mtsevent;
+
+public class AccountData {
+    private String name;
+    private String surname;
+
+    AccountData instance;
+
+    public AccountData(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public AccountData getInstance(){
+
+    }
+
+}
